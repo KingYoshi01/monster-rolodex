1ab2ec7fe4e8c83d902a1b2c9252025225d73797
robohacker <King_Yoshi@gmx.com>
2021-04-13 16:03:42 -0400

    initial files for github pages

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './search-box.styles.css';
+
+export const SearchBox = ({ handleChange, placeholder }) => (
+  <input
+    type="search"
+    className="search"
+    placeholder={placeholder}
+    onChange={handleChange}
+  />
+);
